3c273d442e0f5277040ce016a5ce0fa7a4b54b10
xzx <xzx>
2018-07-11 18:51:47 +0800

    fix[7/11]:toRem修复

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,9 @@
   <div>
     <header class="header">
       <!--头部-->
-      <div><i class="iconfont icon-all"></i></div>
+      <div>
+        <i class="iconfont icon-all"></i>
+      </div>
       首页
     </header>
     <article>
@@ -10,183 +12,235 @@
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
       <div class="news">
         <h5>iView 3.0 啦</h5>
         <p>最新消息！！！,iview团队发布3.0,将在北京举行发布会。巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉巴拉...</p>
         <div>
-          <span><i class="iconfont icon-account"></i> 陆嘿</span>
-          <span><i class="iconfont icon-calendar"></i> 2018-05-20</span>
-          <span><i class="iconfont icon-hot"></i> 2452已读</span>
-          <span><i class="iconfont icon-good-filling"></i> 1000赞</span>
+          <span>
+            <i class="iconfont icon-account"></i> 陆嘿</span>
+          <span>
+            <i class="iconfont icon-calendar"></i> 2018-05-20</span>
+          <span>
+            <i class="iconfont icon-hot"></i> 2452已读</span>
+          <span>
+            <i class="iconfont icon-good-filling"></i> 1000赞</span>
         </div>
       </div>
     </article>
     <nav class="navList">
-      <span><i class="iconfont icon-hot"></i></span>
-      <span><i class="iconfont icon-category"></i></span>
-      <span><i class="iconfont icon-form"></i></span>
-      <span><i class="iconfont icon-account"></i></span>
+      <span>
+        <i class="iconfont icon-hot"></i>
+      </span>
+      <span>
+        <i class="iconfont icon-category"></i>
+      </span>
+      <span>
+        <i class="iconfont icon-form"></i>
+      </span>
+      <span>
+        <i class="iconfont icon-account"></i>
+      </span>
     </nav>
   </div>
 </template>
 <style lang="scss" scoped>
 @import "@/assets/css/_benchmark.scss";
-$initBgColor:#4a8bce;
-$initColor:#4e4b4b;
-  .header{
-    font-size:px2rem(22px);
-    height:px2rem(60px);
-    line-height:px2rem(60px);
-    background:$initBgColor;
-    text-align:center;
-    color:#fff;
-    position:relative;
-    & :nth-child(1){
-      height:px2rem(60px);
-      width:px2rem(50px);
-      display:inline-block;
-      position:absolute;
-      left:0px;
-    }
-    // & :nth-child(2){
-    //   display:inline-block;
-    //   text-align:center;
-    //   width:px2rem(320px);
-    //   float:left;
-    // }
-  }
-  .news{
-    padding:px2rem(5px) px2rem(10px);
-    margin:px2rem(5px);
-    background:#fff;
-    h5{
-      font-size:px2rem(18px);
-      margin-bottom:px2rem(5px);
-    }
-    p{
-      margin-bottom:px2rem(5px);
-      font-size:13px;
-      color:$initColor;
-      text-indent:2em;
-      margin-bottom:px2rem(5px);
-    }
-    div,i{
-      color:#748594;
-      font-size:12px;
-      span{
-        margin-right:0.5em;
-      }
+$initBgColor: #4a8bce;
+$initColor: #4e4b4b;
+.header {
+  font-size: px2rem(22px);
+  height: px2rem(60px);
+  line-height: px2rem(60px);
+  background: $initBgColor;
+  text-align: center;
+  color: #fff;
+  position: relative;
+  & :nth-child(1) {
+    height: px2rem(60px);
+    width: px2rem(50px);
+    display: inline-block;
+    position: absolute;
+    left: 0px;
+    i {
+      font-size: px2rem(16px);
     }
   }
-  .navList{
-    // width: px2rem(375px);
-    position:fixed;
-    bottom:0;
-    width:100%;
-    background: #fff;
-    text-align:center;
-    height:px2rem(60px);
-    line-height:px2rem(60px);
-    font-weight:bold;
-    border-top:px2rem(1px) solid #cdcdcd;
-    border-top-left-radius:45%;
-    border-top-right-radius:45%;
-    background:$initBgColor;
-    overflow:hidden;
-    span{
-      display:inline-block;
-      width:25%;
-      float:left;
-      color:#fff;
-      i{
-        font-size:px2rem(20px);
-      }
+  // & :nth-child(2){
+  //   display:inline-block;
+  //   text-align:center;
+  //   width:px2rem(320px);
+  //   float:left;
+  // }
+}
+.news {
+  padding: px2rem(5px) px2rem(10px);
+  margin: px2rem(5px);
+  background: #fff;
+  h5 {
+    font-size: px2rem(18px);
+    margin-bottom: px2rem(5px);
+  }
+  p {
+    margin-bottom: px2rem(5px);
+    font-size: px2rem(13px);
+    color: $initColor;
+    text-indent: 2em;
+    margin-bottom: px2rem(5px);
+  }
+  div,
+  i {
+    color: #748594;
+    font-size: px2rem(12px);
+    span {
+      margin-right: 0.5em;
     }
   }
+}
+.navList {
+  // width: px2rem(375px);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: #fff;
+  text-align: center;
+  height: px2rem(60px);
+  line-height: px2rem(60px);
+  font-weight: bold;
+  border-top: px2rem(1px) solid #cdcdcd;
+  border-top-left-radius: 45%;
+  border-top-right-radius: 45%;
+  background: $initBgColor;
+  overflow: hidden;
+  span {
+    display: inline-block;
+    width: 25%;
+    float: left;
+    color: #fff;
+    i {
+      font-size: px2rem(20px);
+    }
+  }
+}
 </style>
