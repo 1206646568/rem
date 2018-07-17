a42d09606d82a3b1b52c7345b26d734f615a804c
xzx <1206646568@qq.com>
2018-07-17 08:43:47 +0800

    feat:[7/07]:route

---
+++ src/components/public/theQuickWay.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <header class="header">
+      <!--头部-->
+      <div>
+        <i class="iconfont icon-all"></i>
+      </div>
+      首页
+    </header>
+    <router-view></router-view>
+    <nav class="navList">
+      <a>
+        <i class="iconfont icon-hot"></i>
+        <span>热门</span>
+      </a>
+      <a>
+        <i class="iconfont icon-category"></i>
+        <span>文章</span>
+      </a>
+      <a>
+        <i class="iconfont icon-form"></i>
+        <span>分类</span>
+      </a>
+      <a>
+        <i class="iconfont icon-account"></i>
+        <span>用户</span>
+      </a>
+    </nav>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "@/assets/css/_benchmark.scss";
+$initBgColor: #4a8bce;
+$initColor: #4e4b4b;
+.header {
+  font-size: px2rem(22px);
+  height: px2rem(60px);
+  line-height: px2rem(60px);
+  background: $initBgColor;
+  text-align: center;
+  color: #fff;
+  position: relative;
+  & :nth-child(1) {
+    height: px2rem(60px);
+    width: px2rem(50px);
+    display: inline-block;
+    position: absolute;
+    left: 0px;
+    i {
+      font-size: px2rem(16px);
+    }
+  }
+  // & :nth-child(2){
+  //   display:inline-block;
+  //   text-align:center;
+  //   width:px2rem(320px);
+  //   float:left;
+  // }
+}
+
+.navList {
+  // width: px2rem(375px);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: #fff;
+  text-align: center;
+  // height: px2rem(60px);
+  // line-height: px2rem(60px);
+  // font-weight: bold;
+  border-top: px2rem(1px) solid #cdcdcd;
+  // border-top-left-radius: 45%;
+  // border-top-right-radius: 45%;
+  background: $initBgColor;
+  overflow: hidden;
+  a {
+    display: inline-block;
+    width: 25%;
+    float: left;
+    color: #fff;
+    i {
+      font-size: px2rem(16px);
+    }
+    span{
+      font-size: px2rem(12px);
+      display:block;
+    }
+
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import login from '@/views/login'
+// import login from '@/views/login'
 import home from '@/views/home'
+import theQuickWay from '@/components/public/theQuickWay'
 
 Vue.use(Router)
 
 let VueRouter = new Router({
   mode: 'history',
-  routes: [{
-    path: '/',
-    name: 'login',
-    component: login
-  },{
-    path: '/home',
-    name: 'home',
-    component: home
-  }]
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'login',
+    //   component: login
+    // },
+    {
+      path:'/',
+      component: theQuickWay,
+      redirect:'home',
+      children:[
+        {
+          path: '/home',
+          name: 'home',
+          component: home
+        }
+      ]
+    }
+    // { path: '*', redirect: { name: 'index' } },
+]
 })
 VueRouter.beforeEach((to, from, next) => {
   next()
