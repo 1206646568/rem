f5b5e69ae6f36e6a446aa2cc720475e689f7dfd4
xzx <1206646568@qq.com>
2018-07-23 23:36:03 +0800

    components

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,6 +3,17 @@
    <span>首页 </span>
   </div>
 </template>
+<script>
+import btn '@/components/core/btn' 
+export defalut{
+  components:{btn},
+  data(){
+    return {
+
+    }
+  }
+}
+</sript>
 <style lang="scss" scoped>
 .home {
   font-size: 20px;
